1e8c3c95f7db6c7f57d3a5e2b9c0226f350862b9
shams-shifl <109410388+shams-shifl@users.noreply.github.com>
2025-03-08 01:26:37 +0600

    Fixed data, svg code and tooltip position

---
+++ React App Testing/trade-deficit-map-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TradeDeficitMap from './TradeDeficitMap';
+
+function App() {
+  return (
+    <div className="App">
+      <TradeDeficitMap />
+    </div>
+  );
+}
+
+export default App;
